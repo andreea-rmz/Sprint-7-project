f7def12add99f132ebfcddf3b0e20e34f138b542
Andrea Ramirez <andreanayelirb@gmail.com>
2025-01-29 15:58:17 -0600

    Avance 1

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+fastfood_data = pd.read_csv("/Users/andrearmz/Documents/Triple Ten/Sprint 7/Sprint-7-project/fastfood_data.csv")

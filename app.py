5da2c45084a77ce73dad6ccd7601008f9263f223
Andrea Ramirez <andreanayelirb@gmail.com>
2025-02-01 13:59:17 -0600

    Add histogram & scatter plot to the if block

--- app.py
+++ app.py
@@ -23,9 +23,8 @@ histogram_button = st.button("Show histogram")
 
 if histogram_button:
     st.write("Histogram that shows count of observations by restaurant chain")
-
-histogram = px.histogram(fastfood_data, x="Company") # create histogram
-st.plotly_chart(histogram, use_container_width=True)
+    histogram = px.histogram(fastfood_data, x="Company") # create histogram
+    st.plotly_chart(histogram, use_container_width=True)
 
 # Create a checkbox that when checked, builds a scatter plot
 scatter_box = st.checkbox("Show scatter plot")
@@ -33,8 +32,8 @@ scatter_box = st.checkbox("Show scatter plot")
 if scatter_box:
     st.write("Scatter plot for the data")
    
-scatter = px.scatter(fastfood_data, x="Item", y="Calories") # create scatter plot
-st.plotly_chart(scatter, use_container_width=True)
+    scatter = px.scatter(fastfood_data, x="Item", y="Calories") # create scatter plot
+    st.plotly_chart(scatter, use_container_width=True)
 
 
 
